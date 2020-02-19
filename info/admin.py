d21ccededb47273c53d78525248f367f6495c6e1
Madiyor <abdukhashimov@yandex.ru>
2020-02-19 14:59:26 +0500

    Made a model

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from info.models import Info
 
-# Register your models here.
+admin.site.register(Info)
--- info/models.py
+++ info/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Info(models.Model):
+    name = models.CharField(max_length=255)
+    information = models.TextField()
+
+    def __str__(self):
+        if len(str(self.name)) > 20:
+            return str(self.name)[:20] + '...'
+        return self.name
