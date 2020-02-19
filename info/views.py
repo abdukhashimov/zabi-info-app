78d7b13f445f42dd653c2833d89b695ea1fd10a3
Madiyor <abdukhashimov@yandex.ru>
2020-02-19 15:05:43 +0500

    added views

--- info/views.py
+++ info/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from info.models import Info
+from info.serializers import InfoSerializer
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView
+)
 
-# Create your views here.
+
+class InfoListApiView(ListAPIView):
+    queryset = Info.objects.all()
+    serializer_class = InfoSerializer
+    permission_classes = [AllowAny, ]
+
+
+class InfoCreateApiView(CreateAPIView):
+    queryset = Info.objects.all()
+    serializer_class = InfoSerializer
+    permission_classes = [IsAdminUser, ]
+
+class InfoDetailUpdateDeleteApiView(RetrieveUpdateDestroyAPIView):
+    queryset = Info.objects.all()
+    serializer_class = InfoSerializer
+    permission_classes = [IsAdminUser, ]
